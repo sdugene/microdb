00add2a98eba6452f1e62e1f20499e31221fea31
Webmaster SiteOffice <webmaster@siteoffice.fr>
2016-02-04 21:48:37 +0100

    add findWithJoin function

--- src/EntityManager.php
+++ src/EntityManager.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace MicroDB;
+use Engine\Functions\Object;
 
 /**
  * Description of EntityManager
@@ -10,6 +11,12 @@ namespace MicroDB;
 class EntityManager
 {
 	private static $_instance = null;
+	
+	protected $joinedProperties = null;
+    protected $reflectionClass = null;
+    protected $properties = null;
+    protected $mapping = null;
+    protected $class = null;
 	
 	private $conf = [];
 	private $entity = null;
@@ -40,18 +47,178 @@ class EntityManager
     public function entity($entity)
     {
     	$this->entity = $entity;
-    	$this->database->setPath($this->folder.$entity);
+    	if (is_object($entity)) {
+	    	$this->class = get_class($this->entity);
+	    	$this->getClassAnnotations();
+	    	$this->database->setPath($this->folder.$this->entity->getClassName());
+    	} else {
+    		$this->database->setPath($this->folder.$entity);
+    	}
     	return $this;
     }
-    
-    public function find($id)
+
+    /**
+     * @param int|array $id|$criteria
+     * @param array|int $join|$limit
+     * @param int|array $limit|$order
+     * @param array $order|$group
+     * @param array $group
+     * @return array
+     */
+    public function find()
     {
-    	return $this->database->load($id);
+        /**
+         *  $args[0] : $criteria
+         *  $args[1] : $join
+         *  $args[2] : $limit
+         *  $args[3] : $order
+         *  $args[4] : $group
+         */
+        $args = $this->getArgs(func_get_args());
+        
+        if (is_numeric($args[0])) {
+            return $this->findById($args[0]);
+        }
+        
+        if ($args[0] == '*') {
+        	return $this->findByCriteria();
+        }
+        
+        if (is_array($args[1]) && !empty($args[1])) {
+            return $this->findWithJoin($args[0], $args[1], $args[2], $args[3], $args[4]);
+        }
+        
+        if (is_array($args[0])) {
+            return $this->findByCriteria($args[0], $args[1], $args[2], $args[3]);
+        }
+    }
+    
+    private function findById($id)
+    {
+    	$result = $this->database->load($id);
+    	return Object::fillWithJSon($this->entity, json_encode($result));
+    }
+
+    /**
+     * @param array $criteria
+     * @param bool|false $maxLine
+     * @param bool|false $order
+     * @param bool|false $group
+     * @return array
+     */
+    private function findByCriteria($criteria = [], $maxLine = false, $order = false, $group = false)
+    {
+    	$results = [];
+    	$array = $this->database->find($criteria);
+    	foreach ($array as $key => $value) {
+    		if ($key == $maxLine) {
+    			break;
+    		}
+    		$results[] = Object::fillWithJSon($this->entity, json_encode($value));
+    	}
+    	return $results;
+    }
+
+    /**
+     * @param array $criteria
+     * @param array $join
+     * @param bool|false $maxLine
+     * @param bool|false $order
+     * @param bool|false $group
+     * @return array
+     */
+    private function findWithJoin($criteria = [], $join = [], $maxLine = false, $order = false, $group = false)
+    {
+        $results = [];
+        $i = 0;
+    	$array = $this->database->find($criteria);
+    	foreach ($array as $key => $value) {
+    		if ($key == $maxLine) {
+    			break;
+    		}
+    		$results[$i] = Object::fillWithJSon($this->entity, json_encode($value));
+    		$results[$i] = $this->join($join, $results[$i]);
+    		$i++;
+    	}
+    	return $results;
+    }
+
+    /**
+     * @param $args
+     * @param int $max
+     * @return mixed
+     */
+    private function getArgs($args, $max = 5)
+    {
+        for($j = 0 ; $j < $max ; $j++) {
+            if (!array_key_exists($j, $args) && $j < 2) {
+                $args[$j] = [];
+            } elseif (!array_key_exists($j, $args)) {
+                $args[$j] = false;
+            }
+        }
+        return $args;
+    }
+
+    /**
+     * @return void
+     */
+    private function getClassAnnotations()
+    {
+        $this->mapping = Mapping::getReader($this->class);
+        $this->properties = $this->mapping->getPropertiesMapping();
+        $this->joinedProperties = $this->mapping->getPropertiesMapping('Joined');
     }
     
     public function insert($input)
     {
     	return $this->database->copy($input);
+    }
+    
+    private function join($join, $result)
+    {
+    	foreach ($join as $method => $joinArray) {
+            $className = key($joinArray);
+            $joinCriteria = $this->joinCriteria($joinArray[$className], $className, $result);
+            $this->database->setPath($this->folder.ucfirst($className));
+            $resultJoin = $this->database->find($joinCriteria);
+            
+            if (count($resultJoin) == 1) {
+            	$resultJoin = $resultJoin[key($resultJoin)]; 
+            }
+            
+            foreach($this->joinedProperties as $property => $needed) {
+            	if (preg_match('/'.$className.'_([a-z_-]*)/', $needed, $infos) && array_key_exists($infos[1], $resultJoin)) {
+            		$result->$property = $resultJoin[$infos[1]];
+            	}
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * @param $criteria
+     * @param $table
+     * @return string
+     */
+    private function joinCriteria($criteria, $table, $result)
+    {
+    	$joinCriteria = [];
+    	foreach ($criteria as $boolean => $column) {
+    		if(!is_array($column)) {
+                $joinMapping = $this->mapping->getPropertieJoinColumn($column, $table);
+            }
+            
+            foreach ($joinMapping as $key => $value) {
+            	preg_match('/^@([a-zA-Z_-]*)\.@([a-zA-Z_-]*)/', $key, $matchesKey);
+            	preg_match('/^@([a-zA-Z_-]*)\.@([a-zA-Z_-]*)/', $value, $matchesValue);
+            	if ($this->entity->getClassName() == ucfirst($matchesKey[1])) {
+            		$joinCriteria[$matchesValue[2]] = $this->entity->$matchesKey[2];
+            	}
+            }
+    	}
+    	
+    	return $joinCriteria; 
     }
     
     public function delete($id)
---
+++ src/Mapping.php
@@ -0,0 +1,179 @@
+<?php
+
+namespace MicroDB;
+
+/**
+ * Description of Mapping
+ *
+ * @author Sébastien Dugène
+ */
+class Mapping
+{
+    private $reader = null;
+    private $propertiesMappingColumn = null;
+    private $propertiesMappingJoined = null;
+    private $reflectionClass = null;
+    private $classMapping = [];
+    private $className = null;
+
+    /**
+     * @param $className
+     * @return void
+     */
+    private function __construct($className)
+    {
+        $this->reader = \Minime\Annotations\Reader::createFromDefaults();
+        $this->reflectionClass = new \ReflectionClass($className);
+        $this->className = $className;
+    }
+
+    /**
+     * @param $className
+     * @return Mapping
+     */
+    public static function getReader($className)
+    {
+        return new Mapping($className);
+    }
+
+    /**
+     * @param $key
+     * @return mixed
+     */
+    public function getClassMapping($key)
+    {
+        if (!array_key_exists($key, $this->classMapping)) {
+            $annotations = $this->reader->getClassAnnotations($this->className);
+            $value = $annotations->get($key);
+            $values = json_encode($this->mappingParse($value));
+            $this->classMapping[$key] = json_decode($values);
+        }
+        return $this->classMapping[$key];
+    }
+
+    /**
+     * @param $table
+     * @return mixed
+     */
+    public function getProperties($table)
+    {
+        $nameSpace = $this->reflectionClass->getNamespaceName();
+        $mapping = Mapping::getReader($nameSpace.'\\'.ucfirst($table));
+        return $mapping->getPropertiesMapping();
+    }
+
+    /**
+     * @param $table
+     * @return mixed
+     */
+    public function getName($table)
+    {
+        $nameSpace = $this->reflectionClass->getNamespaceName();
+        $mapping = Mapping::getReader($nameSpace.'\\'.ucfirst($table));
+        return $mapping->getClassMapping('Table')->name;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getPropertiesMapping($type = 'Column')
+    {
+    	$attributeName = "propertiesMapping".$type;
+        if (is_null($this->$attributeName)) {
+            $array = [];
+            $properties = $this->reflectionClass->getProperties();
+            foreach($properties as $property) {
+                $column = $this->reader->getPropertyAnnotations($this->className, $property->name)->get('ORM\\'.$type);
+                if(!is_null($column)) {
+                    $values = $this->mappingParse($column);
+                    $array[$property->name] = $values['name'];
+                }
+            }
+            $this->$attributeName = $array;
+        }
+        return $this->$attributeName;
+    }
+
+    /**
+     * @param $column
+     * @param $table
+     * @return array
+     */
+    public function getPropertieJoinColumn($column, $table) {
+        $joinColumn = $this->reader
+                ->getPropertyAnnotations($this->className, $column)->get('ORM\JoinColumn');
+        
+        if (is_array($joinColumn)) {
+            foreach ($joinColumn as $line) {
+                $values = $this->mappingParse($line);
+                if ($values['name'] == $table) {
+                    return [
+                        '@'.strtolower($this->reflectionClass->getShortName()).'.@'.$column
+                            => '@'.$table.'.@'. $values['referencedColumnName']
+                    ];
+                }
+            }
+        }
+        
+        if(!is_null($joinColumn)) {
+            $values = $this->mappingParse($joinColumn);
+            return [
+                '@'.strtolower($this->reflectionClass->getShortName()).'.@'.$column
+                    => '@'.$table.'.@'. $values['referencedColumnName']
+            ];
+        }
+    }
+
+    /**
+     * @param $tring
+     * @return mixed
+     */
+    public function getValue($tring) {
+        return $this->propertiesMappingColumn[$tring];
+    }
+
+    /**
+     * @param $string
+     * @return array
+     */
+    private function mappingParse($string)
+    {
+        $array = [];
+        $matches = [];
+        preg_match_all('/([a-zA-Z\d]+)="([a-zA-Z\d_\-]+)"|([a-zA-Z\d]+)=({"[a-zA-Z\d_\-]+"})/', $string, $matches, PREG_SET_ORDER) ;
+        foreach ($matches as $value) {
+            if ($value[1] != '') {
+                $array[$value[1]] = $value[2];
+            } elseif ($value[3] != '') {
+                $array[$value[3]] = json_decode(str_replace(array('{','}'),array('[',']'),$value[4]), true);
+            }
+        }
+        return $array;
+    }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    public function valueMapping($string)
+    {
+        $matches = [];
+        if (preg_match('/@([a-zA-Z\d_\-]+).@([a-zA-Z\d_\-]+)/', $string, $matches)) {
+            $table = '';
+            $column = '';
+            if ($matches[1] == 'this') {
+                $table = $this->classMapping['Table']->name;
+                $column = $this->propertiesMapping[$matches[2]];
+            } else {
+                $nameSpace = $this->reflectionClass->getNamespaceName();
+                $mapping = Mapping::getReader($nameSpace.'\\'.ucfirst($matches[1]));
+                $table = $mapping->getClassMapping('Table')->name;
+                $properties = $mapping->getPropertiesMapping();
+                $column = $properties[$matches[2]];
+            }
+            return '`'.$table.'`' . '.' . '`'.$column.'`';
+        } else {
+            return $string;
+        }
+    }
+}
